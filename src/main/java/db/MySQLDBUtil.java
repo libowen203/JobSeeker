7fd7b4aba9d4767c5b6af6dce3273b9bd335116e
libowen203 <48366035+libowen203@users.noreply.github.com>
2020-08-20 01:12:59 -0400

    Update MySQLDBUtil.java

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db;
 
 public class MySQLDBUtil {
-	private static final String INSTANCE = "laiproject-instance.c38e8la6ydzf.us-east-1.rds.amazonaws.com";
+	private static final String INSTANCE = "your_instance";
 	private static final String PORT_NUM = "3306";
-	public static final String DB_NAME = "laiproject";
-	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "dou19891127gu";
+	public static final String DB_NAME = "your_dbname";
+	private static final String USERNAME = "your_username";
+	private static final String PASSWORD = "your_password";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
